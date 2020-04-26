c90e2f70976d3c8b77c8ce3119ed9da6c78e7b23
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2020-04-26 20:28:11 +0200

    Delete dist folder before compiling documentation.

--- doc/compile.js
+++ doc/compile.js
@@ -1,6 +1,9 @@
-const path = require("path"),
+const fs = require("fs"),
+	path = require("path"),
 	parseYamlDocProject = require("yaml-doc").default,
 	applyBootstrapTemplate = require("yaml-doc-bootstrap").default;
 
-const project = parseYamlDocProject(path.resolve(__dirname, "project.yaml"));
-applyBootstrapTemplate(project);
+const project = parseYamlDocProject(path.resolve(__dirname, "project.yaml")),
+	dist = path.resolve(__dirname, "dist");
+fs.rmdirSync(dist, {recursive: true});
+applyBootstrapTemplate(project, dist);
